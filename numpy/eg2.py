520f744c9ff747a9afa778b4899200d911ced2b9
admin <984925332@qq.com>
2023-05-25 13:34:12 +0800

    numpy练习

---
+++ numpy/eg2.py
@@ -0,0 +1,10 @@
+import numpy as np
+ls1 = [10,42,0,-17,30]
+nd1 = np.array(ls1)
+print(nd1)
+print(type(nd1))
+
+ls2 = [[8,-2,0,34,7],[6,7,8,9,10]]
+nd2 = np.array(ls2)
+print(ls2)
+print(type(ls2))
